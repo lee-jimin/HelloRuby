e4fa60243ef26d7673908304834fe8dead56f00f
lee.jimin <lee.jimin@cashslide.co.kr>
2015-06-02 10:12:44 +0900

    v.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capistrano::Application.config.secret_token = '72698e2cad0d6a9381dfcf447166384475eb2aeaf3fa5f56a67df9dd31c0f904e7931dc0b307fe5f3ae7a369ea5976b745eabbfe982e225f39630586d61a78eb'
